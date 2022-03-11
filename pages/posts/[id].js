1571b684e86e973b837810abc69f9f1f54070037
vinguyen <hoangvi1185@gmail.com>
2022-03-11 10:30:26 +0700

    First simple app

---
+++ pages/posts/[id].js
@@ -0,0 +1,15 @@
+import Layout from "../../components/layout";
+
+export default function Post() {
+    return <Layout>
+
+    </Layout>
+}
+
+export async function getStaticPaths() {
+
+}
+
+export async function getStaticProps({ params }) {
+    
+}
